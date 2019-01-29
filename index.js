6df99ffedda47345f90cf3421de752e73dc2654e
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-29 03:14:06 +0000

    Done.

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ function dwarfRollCall(dwarves) {
   for (var i = 0; i < dwarves.length; i++) {
    newDwarves.push(`${i + 1}. ${dwarves[i]}`);
   }
-  return `${newDwarves.join(" ")}`;
+  return newDwarves.join(" ");
 }
 
 function summonCaptainPlanet(planeteerCalls){
-  var newPlaneteerCalls
-  for (var i = 0; i < planeteerCalls.length; i++) 
-    newPlaneteerCalls = planeteerCalls[i].toUpperCase();
+  var uppercase = []
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    uppercase.push(planeteerCalls[i].toUpperCase()+ "!")
+  }
+return uppercase
 }
 
 function longPlaneteerCalls(words) {
