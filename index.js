227308b5e2918a88cbd6e9e8d0dde3d979b15748
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-06 14:32:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
-function dwarfRollCall(dwarves) {
-  var newDwarves = [];
-  for (var i = 0; i < dwarves.length; i++) {
-   newDwarves.push(`${i + 1}. ${dwarves[i]}`);
+function dwarfRollCall(dwarves){
+  var counting = [];
+  for(var i = dwarves.length / 2; i < dwarves.length; i++){
+    counting.push(`${i+1}. ${dwarves[i]}`);
   }
-  return newDwarves.join(" ");
+  debugger;
+  return counting.join(" ");
 }
 
 
 function summonCaptainPlanet(planeteerCalls){
-  var uppercase = [];
-  for (var i = 0; i < planeteerCalls.length; i++) {
-    uppercase.push(planeteerCalls[i].toUpperCase()+ "!");
+  var scream = [];
+  for(var i = 0; i < planeteerCalls.length; i++){
+    scream.push(planeteerCalls[i].toUpperCase()+"!");
   }
-return uppercase;
+  return scream;
 }
 
 
-function longPlaneteerCalls(words) {
-  for (var i = 0; i < words.length; i++) {
-    if (words[i].length > 4) {
-      return true 
+function longPlaneteerCalls(calls){
+  for(var i = 0; i < calls.length; i++){
+    if(calls[i].length > 4){
+      return true
     }
   }
   return false
 }
 
 
-
-
-function findTheCheese(foods) {
-  var cheese = ["cheddar", "gouda", "camembert"];
-  for (var i = 0; i < foods.length; i++) {
-    for (var x = 0; x < cheese.length; x++) {
+function findTheCheese(foods){
+  var cheese = ["cheddar", "gouda", "camembert"]
+  for (var i = 0; i <foods.length; i++){
+    for (var x = 0; i <cheese.length; i++){
       if (foods[i] === cheese[x]){
-       return cheese[x];
+        return foods[i]
       }
     }
   }
-  return "no cheese!";
+  return "no cheese!"
+}
+
+function wordsWithB(words){
+  var bWords = []
+  for (var i = 0; i < words.length; i++){
+    if (words[i].startsWith("b")){
+     bWords.push(words[i])
+    }
+  }
+  return bWords
 }
